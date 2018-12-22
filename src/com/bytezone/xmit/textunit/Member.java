41b99b71497bdca13dcba4045dd91b106c504be1
Denis Molony <dmolony@iinet.net.au>
2018-12-22 16:01:44 +1100

    Member TextUnit

---
+++ src/com/bytezone/xmit/textunit/Member.java
@@ -0,0 +1,31 @@
+package com.bytezone.xmit.textunit;
+
+public class Member extends TextUnit
+{
+  public final String datasetName;
+
+  public Member (byte[] buffer, int ptr)
+  {
+    super (buffer, ptr);
+
+    StringBuilder text = new StringBuilder ();
+    for (int i = 0; i < dataList.size (); i++)
+    {
+      text.append (dataList.get (i).text);
+      text.append (", ");
+    }
+    text.deleteCharAt (text.length () - 1);
+    text.deleteCharAt (text.length () - 1);
+    datasetName = text.toString ();
+  }
+
+  // ---------------------------------------------------------------------------------//
+  // toString
+  // ---------------------------------------------------------------------------------//
+
+  @Override
+  public String toString ()
+  {
+    return String.format ("%04X  %-8s  %s", keys[keyId], mnemonics[keyId], datasetName);
+  }
+}
