f1489ed2051e22ae69611981056445f45ac8e06f
Denis Molony <dmolony@iinet.net.au>
2019-05-01 14:06:46 +1000

    tidying

--- src/com/bytezone/xmit/gui/DatasetStatus.java
+++ src/com/bytezone/xmit/gui/DatasetStatus.java
@@ -5,11 +5,7 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.TreeMap;
 
-import com.bytezone.xmit.CatalogEntry;
-import com.bytezone.xmit.DataFile;
-import com.bytezone.xmit.Dataset;
-import com.bytezone.xmit.PdsDataset;
-import com.bytezone.xmit.PdsMember;
+import com.bytezone.xmit.*;
 
 // ----------------------------------------------------------------------------------- //
 class DatasetStatus
@@ -33,33 +29,28 @@ class DatasetStatus
     this.dataset = dataset;
     this.name = name;
     catalogEntry = null;
+    dataFile = null;
 
-    if (dataset != null && dataset.isPds ())
-    {
-      String datasetName = dataset.getReader ().getFileName ();
-      if (!datasets.containsKey (datasetName))
-        datasets.put (datasetName, (PdsDataset) dataset);
-    }
-    //    else
-    //    {
-    //      catalogEntry = null;
-    //    }
+    if (dataset != null)
+      if (dataset.isPds ())
+      {
+        String datasetName = dataset.getReader ().getFileName ();
+        if (!datasets.containsKey (datasetName))
+          datasets.put (datasetName, (PdsDataset) dataset);
+      }
+      else if (dataset.isPs ())
+      {
+        dataFile = ((PsDataset) dataset).getFlatFile ();
+      }
   }
 
   //----------------------------------------------------------------------------------- //
   void catalogEntrySelected (CatalogEntry catalogEntry)
   //----------------------------------------------------------------------------------- //
   {
-    //    if (dataset == null || dataset.isPs ())
-    //      return;
-
     this.catalogEntry = catalogEntry;
 
-    if (catalogEntry == null)
-    {
-      dataFile = null;
-    }
-    else
+    if (catalogEntry != null)
     {
       dataFile = catalogEntry.getMember ();
       selectedMembers.put (dataset, catalogEntry.getMemberName ());
@@ -97,9 +88,11 @@ class DatasetStatus
   {
     StringBuilder text = new StringBuilder ();
 
+    String dataFileName = dataFile == null ? "" : dataFile.getName ();
+
     text.append (String.format ("Name............. %s%n", name));
     text.append (String.format ("Dataset.......... %s%n", dataset));
-    text.append (String.format ("Datafile......... %s%n", dataFile.getName ()));
+    text.append (String.format ("Datafile......... %s%n", dataFileName));
     text.append (String.format ("CatalogEntry..... %s", catalogEntry));
 
     return text.toString ();
